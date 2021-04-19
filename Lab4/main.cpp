7d885fa62354db95d1dcf820e2b42ded915cc3af
vaniapril <vaniapril@mail.ru>
2021-04-19 10:14:41 +0300

    chance ctime to chrono

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <ctime>
+#include <chrono>
 #include <random>
 #include "task1.h"
 #include "task2-1.h"
@@ -49,21 +49,21 @@ bool equals(double **A, double **B){
 int main() {
     read();
 
-    clock_t time1 = clock();
+    auto time1 = std::chrono::steady_clock::now();
     double **C1 = namespaceTask1::task(matrixData);
-    clock_t time2 = clock();
+    auto time2 = std::chrono::steady_clock::now();
     double **C21 = namespaceTask2_1::task(matrixData, threadCount);
-    clock_t time3 = clock();
+    auto time3 = std::chrono::steady_clock::now();
     double **C22 = namespaceTask2_2::task(matrixData, threadCount);
-    clock_t time4 = clock();
+    auto time4 = std::chrono::steady_clock::now();
     double **C23 = namespaceTask2_3::task(matrixData, threadCount);
-    clock_t time5 = clock();
+    auto time5 = std::chrono::steady_clock::now();
 
     if(equals(C1, C21) && equals(C21, C22) && equals(C22, C23)){
-        std::cout<< "Simple multiplication: " << (double)(time2 - time1) / CLOCKS_PER_SEC << " sec."<<'\n';
-        std::cout<< "Multithreaded multiplication (row-column): " << (double)(time3 - time2) / CLOCKS_PER_SEC << " sec."<<'\n';
-        std::cout<< "Multithreaded multiplication (column-row) " << (double)(time4 - time3) / CLOCKS_PER_SEC << " sec."<<'\n';
-        std::cout<< "Multithreaded multiplication (blocks): " << (double)(time5 - time4) / CLOCKS_PER_SEC << " sec."<<'\n';
+        std::cout<< "Simple multiplication: " << (double)std::chrono::duration_cast<std::chrono::milliseconds>(time2 - time1).count() / 1000<< " sec."<<'\n';
+        std::cout<< "Multithreaded multiplication (row-column): " << (double)std::chrono::duration_cast<std::chrono::milliseconds>(time3 - time2).count() / 1000<< " sec."<<'\n';
+        std::cout<< "Multithreaded multiplication (column-row) " << (double)std::chrono::duration_cast<std::chrono::milliseconds>(time4 - time3).count() / 1000<< " sec."<<'\n';
+        std::cout<< "Multithreaded multiplication (blocks): " << (double)std::chrono::duration_cast<std::chrono::milliseconds>(time5 - time4).count() / 1000<< " sec."<<'\n';
     } else {
         std::cout<<"Error";
     }
